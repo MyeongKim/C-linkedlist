c8bb3514934ca8b994d6658a5c5c3d5a96b1bbf9
BeomJun Kim <alchemist85k@gmail.com>
2014-12-08 15:54:42 +0900

    delete node
    create delete function

--- linkedlist_C/linkedlist_C/Main.c
+++ linkedlist_C/linkedlist_C/Main.c
@@ -6,6 +6,15 @@ int main()
 
 	addNode(1);
 	addNode(2);
+	addNode(3);
+	printNode();
+	printf("\n");
+
+	deleteNode(2);
+	printNode();
+	printf("\n");
+
+	addNode(4);
 	printNode();
 
 	return 0;
--- linkedlist_C/linkedlist_C/node.h
+++ linkedlist_C/linkedlist_C/node.h
@@ -4,11 +4,12 @@
 typedef struct node
 {
 	int val;
-	struct node* node;
+	struct node* next;
 } NODE;
 
 NODE*	newNode	= NULL;
 NODE*	pHEAD	= NULL;
+NODE*	pDELETE	= NULL;
 
 void addNode(int _val)
 {
@@ -16,8 +17,8 @@ void addNode(int _val)
 	newNode = (NODE*)malloc(sizeof(NODE));
 	newNode->val = _val;
 	
-	if (pHEAD == NULL) { newNode->node = NULL; }
-	else { newNode->node = pHEAD; }
+	if (pHEAD == NULL) { newNode->next = NULL; }
+	else { newNode->next = pHEAD; }
 
 	pHEAD = newNode;
 	
@@ -26,16 +27,40 @@ void addNode(int _val)
 
 void printNode()
 {
-	while (pHEAD != NULL)
+	NODE* searchNODE = pHEAD;
+	while (searchNODE != NULL)
 	{
-		printf("%d\n", pHEAD->val);
-		pHEAD = pHEAD->node;
+		printf("%d\n", searchNODE->val);
+		searchNODE = searchNODE->next;
 	}
 }
 
-void deleteNode()
+void deleteNode(int _val)
 {
+	NODE* searchNODE = pHEAD;
+	while (searchNODE != NULL)
+	{
+		if (searchNODE->val == _val)
+		{
+			pHEAD->next = searchNODE->next;
+			searchNODE->next = NULL;
+			pDELETE = searchNODE;
 
+			break;
+		}
+
+		pHEAD = searchNODE;
+		searchNODE = searchNODE->next;
+	}
+
+	if (pDELETE == NULL)
+	{
+		printf("no search value!!!\n");
+	}
+	else
+	{
+		free(pDELETE);
+	}
 }
 
 void modifyNode()
